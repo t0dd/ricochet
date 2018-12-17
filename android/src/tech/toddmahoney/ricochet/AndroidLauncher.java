1fb1ae912ce0b0139b417c1cf49f336401f93a3b
Todd Mahoney <todd.developer@gmail.com>
2018-12-16 21:01:04 -0700

    Initial commit

---
+++ android/src/tech/toddmahoney/ricochet/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package tech.toddmahoney.ricochet;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tech.toddmahoney.ricochet.GameApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameApp(), config);
+	}
+}
---
+++ desktop/src/tech/toddmahoney/ricochet/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.toddmahoney.ricochet.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.toddmahoney.ricochet.GameApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameApp(), config);
+	}
+}
