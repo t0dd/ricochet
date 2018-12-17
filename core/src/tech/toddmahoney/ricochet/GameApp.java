9669ef3f7a947036de40996fefba6c54dde9413e
Todd Mahoney <todd.developer@gmail.com>
2018-12-16 23:41:14 -0700

    loading a few assets

--- core/src/tech/toddmahoney/ricochet/GameApp.java
+++ core/src/tech/toddmahoney/ricochet/GameApp.java
@@ -2,32 +2,37 @@ package tech.toddmahoney.ricochet;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameApp extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    private Texture redTankImage;
+    private Texture blueTankImage;
+    private Sound cannonSound;
+    private Sound ricochetSound;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        //load the images
+		redTankImage = new Texture(Gdx.files.internal("blue_tank.png"));
+		blueTankImage = new Texture(Gdx.files.internal("red_tank.png"));
+        // load the sounds
+        cannonSound = Gdx.audio.newSound(Gdx.files.internal("cannon.mp3"));
+        cannonSound = Gdx.audio.newSound(Gdx.files.internal("rico.mp3"));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
