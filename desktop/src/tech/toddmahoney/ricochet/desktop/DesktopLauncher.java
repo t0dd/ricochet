2e93d03a7353cecf6593aee56ad85c365033ffff
Todd Mahoney <todd.developer@gmail.com>
2018-12-16 21:50:59 -0700

    Setting up starter classes

--- desktop/src/tech/toddmahoney/ricochet/desktop/DesktopLauncher.java
+++ desktop/src/tech/toddmahoney/ricochet/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import tech.toddmahoney.ricochet.GameApp;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ricochet";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new GameApp(), config);
 	}
 }
