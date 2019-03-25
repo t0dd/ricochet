277b4ac043250093e189ac951b6142aaded768c7
Todd Mahoney <todd.developer@gmail.com>
2019-03-24 18:19:23 -0600

    building out ActorRicochet class

--- core/src/tech/toddmahoney/ricochet/ActorRicochet.java
+++ core/src/tech/toddmahoney/ricochet/ActorRicochet.java
@@ -1,5 +1,10 @@
 package tech.toddmahoney.ricochet;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 /**
@@ -7,4 +12,49 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
  *  Actor class stores data such as position and rotation.
  */
 public class ActorRicochet extends Actor {
+
+    private TextureRegion textureRegion;
+    private Rectangle rectangle;
+
+    public ActorRicochet(){
+        super();
+        textureRegion = new TextureRegion();
+        rectangle = new Rectangle();
+    }
+    public void setTexture(Texture t)
+    {
+        textureRegion.setRegion(t);
+        this.setSize( t.getWidth(), t.getHeight() );
+        rectangle.setSize(t.getWidth(), t.getHeight());
+    }
+
+    public Rectangle getRectangle()
+    {
+        rectangle.setPosition( this.getX(), this.getY() );
+        return rectangle;
+    }
+
+    public boolean overlaps(ActorRicochet other)
+    {
+        return this.getRectangle().overlaps( other.getRectangle() );
+    }
+
+    public void act(float dt)
+    {
+        super.act(dt);
+    }
+
+    public void draw(Batch batch, float parentAlpha)
+    {
+        super.draw( batch, parentAlpha );
+
+        Color c = getColor(); // used to apply tint color effect
+
+        batch.setColor(c.r, c.g, c.b, c.a);
+
+        if ( isVisible() )
+            batch.draw( textureRegion,
+                    getX(), getY(), getOriginX(), getOriginY(),
+                    getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation() );
+    }
 }
