89528f18b8588778d6ff04780f1eaeeed84003dc
Todd Mahoney <todd.developer@gmail.com>
2019-03-24 18:13:14 -0600

    refactoring Actor class to ActorRicochet

---
+++ core/src/tech/toddmahoney/ricochet/ActorRicochet.java
@@ -0,0 +1,10 @@
+package tech.toddmahoney.ricochet;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ *  Extend the Actor class to include graphics and collision detection.
+ *  Actor class stores data such as position and rotation.
+ */
+public class ActorRicochet extends Actor {
+}
